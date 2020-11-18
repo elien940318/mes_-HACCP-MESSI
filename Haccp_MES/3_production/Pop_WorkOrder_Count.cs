cb0a2abfeaa8bbcd98cc033bed670a36738361c4
DESKTOP-SDGQLA2\dolph <dolphin00@naver.com>
2020-11-19 02:14:57 +0900

    Merge branch 'master' of https://github.com/elien940318/SmartIT_Haccp_
    * 'master' of https://github.com/elien940318/SmartIT_Haccp_:
      트리거는 방아쇠란 뜻입니다. 빵야!
      [작업지시 100%, 실적등록 90%]
      탭삭제
      no message
    
    # Conflicts:
    #	Haccp_MES/Home.Designer.cs
    #	Haccp_MES/Home.cs

---
+++ Haccp_MES/3_production/Pop_WorkOrder_Count.cs
@@ -0,0 +1,142 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Haccp_MES._3_production
+{
+    public partial class Pop_WorkOrder_Count : Form
+    {
+
+        MySqlConnection conn;
+        MySqlCommand cmd;
+        MySqlDataAdapter adapter;
+        DataTable dt;
+
+        string selectedCode; // 콤보박스에서 선택한 품목코드
+
+        public Pop_WorkOrder_Count( )
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Pop_WorkOrder_Count_Load(object sender, EventArgs e)
+        {
+            Prod_1_1_WorkOrders p11w = (Prod_1_1_WorkOrders)this.Owner;
+            this.textBox1.Text = p11w.textBox1.Text;  // 이전 창에서 품목명 가져오기
+            selectedCode = p11w.cbxITEM.Text; // 이전 창에서 품목코드 가져오기
+
+            GridViewFill();
+        }
+
+        // 그리드뷰 데이터 불러오기
+        private void GridViewFill()
+        {
+            conn = new MySqlConnection(DatabaseInfo.DBConnectStr());
+            conn.Open();
+
+          
+            string qry = "select d.mat_no as 'SubMat_no', d.mat_name as 'subMat_name', c.curmat_count as 'NowStock', d.bom_count as 'UnitCnt', (d.bom_count * 0) as 'Needs' " +
+                         "from manage_curmat c,(select a.mat_no, a.mat_name, b.bom_count " +
+                                               "from info_material a,(select bom_no, bom_count " +
+                                                                      "from info_bom " +
+                                                                      "where bom_no != " + selectedCode + " AND bom_parent_no = " + selectedCode + ") b " +
+                                               "where a.mat_no = b.bom_no) d " +
+                          "where c.mat_no = d.mat_no";
+
+
+            cmd = new MySqlCommand(qry, conn);
+            adapter = new MySqlDataAdapter(cmd);
+
+            dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView2.DataSource = dt;
+
+            conn.Close(); 
+        }
+
+        // 숫자외 입력막기
+        private void txtQty_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //숫자와 백스페이스를 제외한 나머지를 바로 처리
+            if (!(char.IsDigit(e.KeyChar) || e.KeyChar == Convert.ToChar(Keys.Back)))   
+            {
+                e.Handled = true;
+            }
+
+        }
+
+        // 지시수량 칸에 숫자를 입력하면...
+        private void txtQty_TextChanged(object sender, EventArgs e)
+        {
+            // 입력창이 비어있는 것도 아니면서 값이 0도 아니면
+            if (txtQty.Text != "" & txtQty.Text != "0") 
+            {
+                btnOk.Enabled = true;
+
+                //int realQty;  // 현재고량
+                //int unitQty;  // 단위소요량
+                //int NeedQty;  // 필요소요량
+
+                double realQty;  // 현재고량
+                double unitQty;  // 단위소요량
+                double NeedQty;  // 필요소요량
+
+                int k = (int)dataGridView2.Rows.Count;  // 그리드뷰에 출력된 행의 갯수
+
+                for (int i = 0; i < k; i++)
+                {
+                    //realQty = (int)dataGridView2.Rows[i].Cells[2].Value; // 현재고량
+                    //unitQty = (int)dataGridView2.Rows[i].Cells[3].Value; // 단위소요량
+                    //NeedQty = unitQty * Convert.ToInt32(txtQty.Text); // 필요소요량
+
+                    realQty = (int)dataGridView2.Rows[i].Cells[2].Value; // 현재고량
+                    unitQty = (double)dataGridView2.Rows[i].Cells[3].Value; // 단위소요량
+                    NeedQty = unitQty * Convert.ToDouble(txtQty.Text); // 필요소요량
+
+                    dataGridView2.Rows[i].Cells[4].Value = NeedQty;
+
+                    if (realQty < NeedQty) // 실제 재고수량 보다 필요수량이 더 많다면
+                    {
+                        dataGridView2.Rows[i].Cells[4].Value = realQty - NeedQty;
+                        dataGridView2.Rows[i].Cells[4].Style.ForeColor = Color.DarkRed;
+
+                        btnOk.Enabled = false; // 마이너스 하나라도 있으면 확인버튼 비활성화
+                    }
+                    
+                }
+            }
+            // 입력창이 공백이거나 0이 입력된 상태면
+            else if (txtQty.Text == "" || txtQty.Text == "0") 
+            {
+                btnOk.Enabled = false;
+
+                int k = (int)dataGridView2.Rows.Count;  // 그리드뷰에 출력된 행의 갯수
+                for (int i = 0; i < k; i++)
+                {
+                    dataGridView2.Rows[i].Cells[4].Value = 0;
+                }
+            }
+        }
+
+        // 확인버튼
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            Prod_1_1_WorkOrders p11w = (Prod_1_1_WorkOrders)this.Owner;
+
+            p11w.textBox2.Text = this.txtQty.Text; // 지시수량을 이전 폼에 넘겨줌
+
+            this.Close(); // 팝업창 닫기
+        }
+
+      
+    }
+}
---
+++ Haccp_MES/3_production/Prod_1-1_WorkOrders.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace Haccp_MES._3_production
+{
+    partial class Prod_1_1_WorkOrders
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.wkDate = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.cbxITEM = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtAdmin = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbxWHS = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(55, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(167, 37);
+            this.label5.TabIndex = 95;
+            this.label5.Text = "작업지시등록";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(227, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 12);
+            this.label1.TabIndex = 97;
+            this.label1.Text = "지시일자";
+            // 
+            // wkDate
+            // 
+            this.wkDate.Location = new System.Drawing.Point(289, 18);
+            this.wkDate.Name = "wkDate";
+            this.wkDate.Size = new System.Drawing.Size(110, 21);
+            this.wkDate.TabIndex = 98;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(421, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 12);
+            this.label2.TabIndex = 99;
+            this.label2.Text = "지시품목";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(479, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(84, 21);
+            this.textBox1.TabIndex = 100;
+            // 
+            // cbxITEM
+            // 
+            this.cbxITEM.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.cbxITEM.FormattingEnabled = true;
+            this.cbxITEM.Location = new System.Drawing.Point(569, 17);
+            this.cbxITEM.Name = "cbxITEM";
+            this.cbxITEM.Size = new System.Drawing.Size(80, 21);
+            this.cbxITEM.TabIndex = 101;
+            this.cbxITEM.SelectedIndexChanged += new System.EventHandler(this.cbxITEM_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label3.Location = new System.Drawing.Point(36, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 12);
+            this.label3.TabIndex = 102;
+            this.label3.Text = "지시수량";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox2.Location = new System.Drawing.Point(95, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(110, 22);
+            this.textBox2.TabIndex = 103;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label4.Location = new System.Drawing.Point(421, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 12);
+            this.label4.TabIndex = 104;
+            this.label4.Text = "입고창고";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox3.Location = new System.Drawing.Point(479, 48);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(85, 21);
+            this.textBox3.TabIndex = 105;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label6.Location = new System.Drawing.Point(8, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 12);
+            this.label6.TabIndex = 107;
+            this.label6.Text = "작업지시번호";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(95, 18);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(110, 21);
+            this.textBox4.TabIndex = 108;
+            this.textBox4.Text = "자동생성";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtAdmin
+            // 
+            this.txtAdmin.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtAdmin.Location = new System.Drawing.Point(290, 48);
+            this.txtAdmin.Name = "txtAdmin";
+            this.txtAdmin.Size = new System.Drawing.Size(109, 21);
+            this.txtAdmin.TabIndex = 110;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label7.Location = new System.Drawing.Point(240, 52);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 12);
+            this.label7.TabIndex = 109;
+            this.label7.Text = "작업자";
+            // 
+            // cbxWHS
+            // 
+            this.cbxWHS.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.cbxWHS.FormattingEnabled = true;
+            this.cbxWHS.Location = new System.Drawing.Point(569, 48);
+            this.cbxWHS.Name = "cbxWHS";
+            this.cbxWHS.Size = new System.Drawing.Size(80, 21);
+            this.cbxWHS.TabIndex = 106;
+            this.cbxWHS.SelectedIndexChanged += new System.EventHandler(this.cbxWHS_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(517, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 23);
+            this.button1.TabIndex = 110;
+            this.button1.Text = "저장";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(605, 168);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 23);
+            this.button2.TabIndex = 111;
+            this.button2.Text = "닫기";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Haccp_MES.Properties.Resources.browser_4x;
+            this.pictureBox1.Location = new System.Drawing.Point(17, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 96;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.txtAdmin);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.wkDate);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.cbxITEM);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.cbxWHS);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Location = new System.Drawing.Point(14, 62);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(672, 92);
+            this.panel1.TabIndex = 112;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel2.Location = new System.Drawing.Point(17, 53);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(669, 1);
+            this.panel2.TabIndex = 113;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel3.Location = new System.Drawing.Point(17, 161);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(669, 1);
+            this.panel3.TabIndex = 114;
+            // 
+            // Prod_1_1_WorkOrders
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(702, 197);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Name = "Prod_1_1_WorkOrders";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "작업지시등록";
+            this.Load += new System.EventHandler(this.Prod_1_1_WorkOrders_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker wkDate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ComboBox cbxWHS;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.ComboBox cbxITEM;
+        public System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtAdmin;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
